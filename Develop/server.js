7cb03ac34275fb357bf570d8ee84e5f924d43088
Eleftherios Moschos <tmoschos@bigpond.com>
2020-07-31 01:08:59 +0930

    Added Post and delete functionality

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const PORT =  3000; //Don't for get to add the heroku path "process.env.PORT ||"
 const fs = require("fs");
-const notesArr = require("./db/db.json")//This needs to go to the db.json file and retrieve data
+const notes = require("./db/db.json");
+// let notes = fs.readFileSync("./db/db.json");//This needs to go to the db.json file and retrieve data
+const PORT =  process.env.PORT || 3000; //Don't for get to add the heroku path "process.env.PORT ||"
+
+app.use(express.static(__dirname + '/public'));
 
 //url body json parsing 
 app.use(express.urlencoded({ extended: true }));
@@ -17,20 +20,39 @@ app.get("/", (req, res) => {
 //route for when user clicks take note
 app.get("/notes", (req, res) => {
     console.log("Notes triggered");
-    console.log(notesArr);
+    // console.log(notes);
      res.sendFile(path.join(__dirname, "/public/notes.html"))
 });
 
 // //API route to notes
-app.get("/api/notes", (req, res) => res.json(notesArr))
+app.get("/api/notes", (req, res) => {
+    res.json(notes)
+    console.log(notes)
+});
 
 // //Save new note
-// app.post("/api/notes", (req, res) => {
-// })
+app.post("/api/notes", (req, res) => {
+    let newNote = {
+        id: notes.length + 1,
+        title: req.body.title,
+        text: req.body.text
+    };
 
-// app.delete("/api/notes", (req, res) => {
+    let noteList = JSON.parse(fs.readFileSync(path.join(__dirname,"./db/db.json"),"utf-8"));
+    noteList.push(newNote);
 
-// })
+    fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
+
+    res.json(noteList)
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+    let delNote = req.params.id
+    let noteList = JSON.parse(fs.readFileSync(path.join(__dirname,"./db/db.json"),"utf-8"))
+    let rmNote = noteList.filter(noteList => noteList.id != delNote)
+    fs.writeFileSync("./db/db.json",JSON.stringify(rmNote))
+    res.json(rmNote);
+});
 
 app.listen(PORT, function () {
     console.log(`the port is listening to ${PORT}`)
